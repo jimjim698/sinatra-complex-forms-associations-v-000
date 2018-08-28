02690cabdda734461e5c1467c2277cd6160b9687
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-28 20:49:04 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,6 +31,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do
+    raise params.inspect
     @pet = Pet.find(params[:id])
   @pet.update(params["pet"])
   if !params["owner"]["name"].empty?
