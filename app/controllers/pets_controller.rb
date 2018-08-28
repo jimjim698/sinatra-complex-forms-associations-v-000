99785c6554c7ca57b3ac3126122f8dbbfc948cae
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-28 20:37:24 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -38,7 +38,7 @@ class PetsController < ApplicationController
       @pet.owner = Owner.create(params[:owner])
       @pet.save
     else
-    @owner = Owner.find(params[:owner][:name])
+    @owner = Owner.find(params[:owner])
     @pet.owner = @owner
     @pet.save
   end
